30dd7ae795d48c92db74db6f268c6dde3b6d09d9
itzsamr <samarathiban@gmail.com>
2024-05-15 11:27:51 +0530

    getter setter - CarLoan

--- entity/CarLoan.py
+++ entity/CarLoan.py
@@ -24,3 +24,19 @@ class CarLoan(Loan):
         )
         self.car_model = car_model
         self.car_value = car_value
+
+    def __str__(self):
+        return f"{super().__str__()}\nCar Model: {self.car_model}\nCar Value: {self.car_value}"
+
+    # Getter and setter methods for attributes
+    def get_car_model(self):
+        return self.car_model
+
+    def set_car_model(self, car_model):
+        self.car_model = car_model
+
+    def get_car_value(self):
+        return self.car_value
+
+    def set_car_value(self, car_value):
+        self.car_value = car_value
