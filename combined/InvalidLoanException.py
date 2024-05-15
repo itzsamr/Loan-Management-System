60026d28c9898186a7050751ad7cc4923a790657
itzsamr <samarathiban@gmail.com>
2024-05-15 13:08:56 +0530

    final commit

--- combined/InvalidLoanException.py
+++
@@ -1,6 +0,0 @@
-class InvalidLoanException(Exception):
-    """Exception raised for invalid loan operations."""
-
-    def __init__(self, message="Invalid loan operation"):
-        self.message = message
-        super().__init__(self.message)
