6cd854cd018037687bb73ff2028c2ad847c1ae2a
itzsamr <samarathiban@gmail.com>
2024-05-15 11:25:22 +0530

    getter setter - customer

--- entity/Customer.py
+++ entity/Customer.py
@@ -6,3 +6,42 @@ class Customer:
         self.phone_number = phone_number
         self.address = address
         self.credit_score = credit_score
+
+    def __str__(self):
+        return f"Customer ID: {self.customer_id}\nName: {self.name}\nEmail: {self.email}\nPhone Number: {self.phone_number}\nAddress: {self.address}\nCredit Score: {self.credit_score}"
+
+    def get_customer_id(self):
+        return self.customer_id
+
+    def set_customer_id(self, customer_id):
+        self.customer_id = customer_id
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, name):
+        self.name = name
+
+    def get_email(self):
+        return self.email
+
+    def set_email(self, email):
+        self.email = email
+
+    def get_phone_number(self):
+        return self.phone_number
+
+    def set_phone_number(self, phone_number):
+        self.phone_number = phone_number
+
+    def get_address(self):
+        return self.address
+
+    def set_address(self, address):
+        self.address = address
+
+    def get_credit_score(self):
+        return self.credit_score
+
+    def set_credit_score(self, credit_score):
+        self.credit_score = credit_score
