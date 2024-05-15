6ab9a8db3a60d1c7ea1edf3416efb737004788e7
itzsamr <samarathiban@gmail.com>
2024-05-15 12:32:16 +0530

    commit DBConnection.py

--- combined/DBConnection.py
+++ combined/DBConnection.py
@@ -1,17 +1,28 @@
 import pyodbc
-from PropertyUtil import PropertyUtil
+import configparser
 
 
 class DBConnection:
     connection = None
 
     @staticmethod
-    def getConnection():
+    def get_connection():
         if DBConnection.connection is None:
-            connection_string = PropertyUtil.getPropertyString()
+            connection_string = DBConnection.get_connection_string()
             try:
                 DBConnection.connection = pyodbc.connect(connection_string)
                 print("Connected to database successfully")
             except pyodbc.Error as e:
                 print(f"Error while connecting to SQL database: {e}")
         return DBConnection.connection
+
+    @staticmethod
+    def get_connection_string():
+        config = configparser.ConfigParser()
+        config.read(
+            "C:/Users/91915/OneDrive - Valliammai Engineering College/Desktop/CarRentalSystem/LoanManagementSystem/combined/config.properties"
+        )
+        server_name = config.get("DATABASE", "server")
+        database_name = config.get("DATABASE", "database")
+        driver = config.get("DATABASE", "driver")
+        return f"DRIVER={{{driver}}};SERVER={server_name};DATABASE={database_name};Trusted_Connection=yes"
