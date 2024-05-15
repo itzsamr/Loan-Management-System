6103a980d2928af3b11cde3cc999b4071c1069e2
itzsamr <samarathiban@gmail.com>
2024-05-15 11:05:44 +0530

    entity.Customer

---
+++ entity/Customer.py
@@ -0,0 +1,8 @@
+class Customer:
+    def __init__(self, customer_id, name, email, phone_number, address, credit_score):
+        self.customer_id = customer_id
+        self.name = name
+        self.email = email
+        self.phone_number = phone_number
+        self.address = address
+        self.credit_score = credit_score
