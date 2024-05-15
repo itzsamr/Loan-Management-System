aab721fc2ef385d0379b39fd1b9ee3f737da335d
itzsamr <samarathiban@gmail.com>
2024-05-15 11:27:05 +0530

    getter setter - HomeLoan

--- entity/HomeLoan.py
+++ entity/HomeLoan.py
@@ -24,3 +24,19 @@ class HomeLoan(Loan):
         )
         self.property_address = property_address
         self.property_value = property_value
+
+    def __str__(self):
+        return f"{super().__str__()}\nProperty Address: {self.property_address}\nProperty Value: {self.property_value}"
+
+    # Getter and setter methods for attributes
+    def get_property_address(self):
+        return self.property_address
+
+    def set_property_address(self, property_address):
+        self.property_address = property_address
+
+    def get_property_value(self):
+        return self.property_value
+
+    def set_property_value(self, property_value):
+        self.property_value = property_value
